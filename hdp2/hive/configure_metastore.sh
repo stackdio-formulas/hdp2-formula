32c5f742a404f23490d02d7123e9d740ccfb8a13
Clark Perkins <clark.perkins@digitalreasoning.com>
2015-02-23 10:55:12 -0600

    Hive metastore configuration fix

--- hdp2/hive/configure_metastore.sh
+++ hdp2/hive/configure_metastore.sh
@@ -10,13 +10,14 @@ y
 y
 EOF
 
+HIVE_VERSION=`hive --version | head -n1 | cut -d ' ' -f2 | cut -d '-' -f1 | cut -d '.' -f2`
 
 # create the metastore database
 SETUPSQL="/tmp/hive_setup.sql"
 cat >$SETUPSQL <<EOF
 CREATE DATABASE metastore;
 USE metastore;
-SOURCE {{pillar.hdp2.hive.home}}/scripts/metastore/upgrade/mysql/hive-schema-0.12.0.mysql.sql;
+SOURCE {{pillar.hdp2.hive.home}}/scripts/metastore/upgrade/mysql/hive-schema-0.$HIVE_VERSION.0.mysql.sql;
 CREATE USER '{{pillar.hdp2.hive.user}}'@'localhost' IDENTIFIED BY '{{pillar.hdp2.hive.metastore_password}}';
 REVOKE ALL PRIVILEGES, GRANT OPTION FROM '{{pillar.hdp2.hive.user}}'@'localhost';
 GRANT SELECT,INSERT,UPDATE,DELETE,LOCK TABLES,EXECUTE ON metastore.* TO '{{pillar.hdp2.hive.user}}'@'localhost';
